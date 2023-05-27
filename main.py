c70d575a0a322e935c8e262089a153fdeda1f7a6
Stalin <rkostalin@gmail.com>
2023-05-26 21:55:55 -0500

    Migrate to API

--- main.py
+++
@@ -1,8 +0,0 @@
-from connection import sockets
-import asyncio
-
-def main():
-    asyncio.run(sockets.start_server())
-
-if __name__ == "__main__":
-    main()
---
+++ nlp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'')
+
+urlpatterns = [
+    path("api/v1/mds", )
+]
