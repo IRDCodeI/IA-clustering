5b8561511970163f7c3d9b886e83535fe65a79dc
RDCode <rkostalin@gmail.com>
2023-06-01 00:46:45 -0500

    charts started

--- app.py
+++ app.py
@@ -7,21 +7,22 @@ from nlp import nlpdocument
 import typing
 
 
-
 class Data(BaseModel):
     data: typing.Any
+
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5173"],
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.post('/nlp/')
+
+@app.post("/nlp/")
 async def nlp(file: Request):
-  data =  await file.json()
-  nlp_file = jsonable_encoder(nlpdocument(data["file"]))
-  return JSONResponse(content=nlp_file)
+    data = await file.json()
+    nlp_file = jsonable_encoder(nlpdocument(data["file"]))
+    return JSONResponse(content=nlp_file)
